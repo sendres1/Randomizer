aa38345863a675177d06ba14b810bac86497b491
sendres1 <sendres1@L11104042784.wctc.msft>
2014-11-05 19:20:44 -0600

    class lab

---
+++ src/Randomizer/Random.java
@@ -0,0 +1,39 @@
+package Randomizer;
+
+
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author sendres1
+ */
+public class Random {
+    public static void main(String[] args) {
+        
+        private List<String> list = new ArrayList<>();
+        
+        // seed value to make truely random
+        Random r = new Random(System.nanoTime());
+    }
+
+    public Random() {
+    }
+
+    private Random(long nanoTime) {
+        throw new UnsupportedOperationException("Not supported yet."); 
+//To change body of generated methods, choose Tools | Templates.
+    }
+
+    
+
+
+}
